1a694aca77f9c690f1ab305b300d45996165ac1e
Jimmy Mercado <jimmy.mercado@live.com>
2017-08-28 16:42:37 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -30,6 +31,26 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.send(404).send();
+    }
+    res.send({todo});
+
+  }, (e) => {
+    res.status(400).send();
+  });
+
+});
+
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
